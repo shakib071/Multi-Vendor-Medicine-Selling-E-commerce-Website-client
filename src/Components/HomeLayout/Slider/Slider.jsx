be40085f20ed4281f2ff74b2e29cb09b8d5e8666
shakib071 <shakibhasan071@gmail.com>
2025-08-14 14:52:36 +0600

    fixed the slider

--- src/Components/HomeLayout/Slider/Slider.jsx
+++ src/Components/HomeLayout/Slider/Slider.jsx
@@ -27,39 +27,6 @@ const Slider = () => {
       <h2 className="text-3xl font-bold text-center  text-blue-600 mb-8">
         🩺 Featured Products
       </h2>
-      {/* <Swiper
-        pagination={{
-          dynamicBullets: true,
-          clickable: true ,
-        }}
-        modules={[Autoplay,Pagination]}
-        spaceBetween={20}
-        slidesPerView={1}
-        loop={true}
-        speed={1500}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-        
-        breakpoints={{
-          640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-          1280: { slidesPerView: 4 },
-        }}
-        className="mySwiper"
-      >
-        {slidesData.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className="bg-white rounded-xl shadow-lg p-3 flex flex-col items-center text-center">
-              <img
-                src={slide.image}
-                alt={slide.name}
-                className="w-full h-40 object-cover rounded-lg mb-4"
-              />
-              <h3 className="font-semibold text-lg text-gray-800 mb-1">{slide.name}</h3>
-              <p className="text-gray-600 text-sm">{slide.description}</p>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
 
         <Swiper
         modules={[Autoplay, Pagination]}
@@ -74,14 +41,21 @@ const Slider = () => {
         >
                 {slidesData.map((slide) => (
                     <SwiperSlide key={slide.id}>
-                    <div className="bg-white rounded-xl shadow-lg p-3 flex flex-col items-center text-center w-full max-w-5xl mx-auto">
-                        <img
-                        src={slide.image}
-                        alt={slide.name}
-                        className=" h-110 object-cover rounded-lg mb-5" 
-                        />
-                        <h3 className="font-semibold text-xl text-gray-800 mb-2">{slide.name}</h3>
+                    <div className="   shadow-lg  flex flex-col items-center text-center w-full  mx-auto">
+                       <div className="bg-white rounded-2xl p-3">
+                         <div className="h-110 w-230  object-cover rounded-lg mb-5">
+                          <img
+                          src={slide.image}
+                          alt={slide.name}
+                          className=" " 
+                          />
+                        </div>
+                        
+                        <div>
+                          <h3 className="font-semibold text-xl text-gray-800 mb-2">{slide.name}</h3>
                         <p className="text-gray-600 text-base">{slide.description}</p>
+                        </div>
+                       </div>
                     </div>
                     </SwiperSlide>
                 ))}
