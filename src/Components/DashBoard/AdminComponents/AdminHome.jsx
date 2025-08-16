7fb23616c21da630c3f0600557a53170c2eb7ab4
shakib071 <shakibhasan071@gmail.com>
2025-08-16 19:40:18 +0600

    added admin home page logics

--- src/Components/DashBoard/AdminComponents/AdminHome.jsx
+++ src/Components/DashBoard/AdminComponents/AdminHome.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { FaDollarSign, FaClock } from 'react-icons/fa';
+import useAllSoldMed from '../../../Hooks/getAllSoldMeds/useAllSoldMed';
+import Loading from '../../Loading/Loading';
 
 const AdminHome = () => {
-  const paidTotal = 15200.5;
-  const pendingTotal = 4300;
+  const {data,isLoading} = useAllSoldMed();
+  // console.log(data);
+  const soldItem = data?.flatMap(item => item.soldItems);
+ 
+
+  const getPaidTotal = () => {
+    const getPaidItem = soldItem?.filter(item => item.paid_status == "paid");
+    // console.log('paid item',getPaidItem);
+    return getPaidItem?.reduce(
+      (sum,item) => sum + (item.price * item.quantity),0
+    ) || 0;
+  }
+
+  const getPendingTotal = () => {
+    const getPendingItems = soldItem?.filter(item => item.paid_status != "paid");
+    return getPendingItems?.reduce(
+      (sum,item) => sum + (item.price * item.quantity),0
+    ) || 0;
+  }
+  
+
+  if(isLoading){
+    return <Loading></Loading>;
+  }
 
   return (
     <div className="max-w-4xl mx-auto p-8 bg-[#ffffff] rounded-xl shadow-lg">
@@ -20,7 +44,7 @@ const AdminHome = () => {
           <div>
             <h3 className="text-lg font-semibold text-green-800 mb-1">Paid Total</h3>
             <p className="text-4xl font-bold text-green-900">
-              ${paidTotal.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+              ${getPaidTotal()?.toFixed(2)}
             </p>
             <p className="text-green-700 text-sm mt-1">Revenue successfully collected</p>
           </div>
@@ -34,7 +58,7 @@ const AdminHome = () => {
           <div>
             <h3 className="text-lg font-semibold text-red-800 mb-1">Pending Total</h3>
             <p className="text-4xl font-bold text-red-900">
-              ${pendingTotal.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+              ${getPendingTotal()?.toFixed(2)}
             </p>
             <p className="text-yellow-700 text-sm mt-1">Revenue awaiting payment</p>
           </div>
