5a10ab84006d94cf9a4509a15c3b1fb1c1248e39
shakib071 <shakibhasan071@gmail.com>
2025-08-18 05:49:05 +0600

    deployed to firebase

--- src/Hooks/AxiosSecure/useAxiosSecure.jsx
+++ src/Hooks/AxiosSecure/useAxiosSecure.jsx
@@ -13,7 +13,7 @@ const useAxiosSecure = () => {
     const navigate = useNavigate();
 
     axiosSecure.interceptors.request.use(config => {
-        config.headers.Authorization = `Bearer ${user.accessToken}`
+        config.headers.Authorization = `Bearer ${user?.accessToken}`
         return config;
     }, error => {
         return Promise.reject(error);
